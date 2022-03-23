135242642561b1d3d0d64f9171d3b3982a670ea0
RuiLong J <jrl-china@hotmail.com>
2022-03-23 15:58:56 -0400

    Add search archive for feeder

--- feed.py
+++ feed.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import xml.etree.ElementTree as ET
 
@@ -5,6 +6,7 @@ import xml.etree.ElementTree as ET
 class Feeder:
     def __init__(self):
         self.rss_url = "https://bangumi.moe/rss/latest"
+        self.search_url = "https://bangumi.moe/api/v2/torrent/search"
 
     def read_latest(self):
         content = requests.get(self.rss_url).content
@@ -16,11 +18,25 @@ class Feeder:
 
         return bangumis
 
-    def read_with_keywords(self, keywords):
+    def read_latest_with_keywords(self, keywords):
         bangumis = self.read_latest()
         return list(
             filter(
                 lambda bangumi: all(map(lambda x: x in bangumi["title"], keywords)),
                 bangumis,
+            )
+        )
+
+    def search_archive(self, query, keywords):
+        resp = requests.post(self.search_url, json={"query": query})
+        torrents = json.loads(resp.content)["torrents"]
+        print(torrents)
+        return list(
+            map(
+                lambda x: {"title": x["title"], "magnet": x["magnet"]},
+                filter(
+                    lambda bangumi: all(map(lambda x: x in bangumi["title"], keywords)),
+                    torrents,
+                ),
             )
         )
