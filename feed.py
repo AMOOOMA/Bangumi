eb5625db8d8e70f917659891844eb6746702de86
RuiLong J <jrl-china@hotmail.com>
2022-03-23 16:20:07 -0400

    Add swap link with magnet func

--- feed.py
+++ feed.py
@@ -3,6 +3,18 @@ import requests
 import xml.etree.ElementTree as ET
 
 
+def swap_link_with_magnet(bangumis):
+    for bangumi in bangumis:
+        link = bangumi.pop("link", None)
+        _id = link.split("/")[-1]
+        torrent = json.loads(
+            requests.post(
+                "https://bangumi.moe/api/torrent/fetch", json={"_id": _id}
+            ).content
+        )
+        bangumi["magnet"] = torrent["magnet"]
+
+
 class Feeder:
     def __init__(self):
         self.rss_url = "https://bangumi.moe/rss/latest"
@@ -30,7 +42,6 @@ class Feeder:
     def search_archive(self, query, keywords):
         resp = requests.post(self.search_url, json={"query": query})
         torrents = json.loads(resp.content)["torrents"]
-        print(torrents)
         return list(
             map(
                 lambda x: {"title": x["title"], "magnet": x["magnet"]},
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from feed import Feeder
 
 def main():
     feeder = Feeder()
-    # feeder.read_latest()
+    print(feeder.read_latest())
     # print(feeder.read_latest_with_keywords(["Lilith-Raws", "秘密內幕 女警的反擊"]))
-    print(feeder.search_archive("Lilith-Raws", ["Lilith-Raws", "秘密內幕 女警的反擊"]))
+    # print(feeder.search_archive("Lilith-Raws", ["Lilith-Raws", "秘密內幕 女警的反擊"]))
 
 
 if __name__ == "__main__":
